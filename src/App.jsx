0e23a6297c9788b9f2cc65a224d73276173e7779
Javier Uncos <javiersrfc30@gmail.com>
2025-10-10 19:18:18 -0300

    quitamos stilos por defecto e instalamos tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-500'>Motomecanica</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
